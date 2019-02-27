cbd1f29fa5ae6357d501b4dae28b8a5f59c45fa0
aidanBailey <aidan.bailey@ucalgary.ca>
2019-02-26 17:58:11 -0700

    Setup NodeJS modules and basic client application.

---
+++ index.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let app = express();
+let http = require('http').Server(app);
+
+app.use(express.static(__dirname + "/client"));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/client/index.html');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
