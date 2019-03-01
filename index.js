f564dc7360582da26ca2f43fc0830f5563c05cbd
aidanBailey <aidan.bailey@ucalgary.ca>
2019-02-28 17:13:09 -0700

    Add basic UI structure

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 let express = require('express');
 let app = express();
 let http = require('http').Server(app);
+let io = require('socket.io')(http);
 
 app.use(express.static(__dirname + "/client"));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/client/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
 });
 
 http.listen(3000, function(){
