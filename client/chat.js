c4d4d9d47db06be5d323c155ce59943fcc16bffb
aidanBailey <aidan.bailey@ucalgary.ca>
2019-03-02 23:10:58 -0700

    Client and Server now have a basic protocol in place to determine the nickname and color of client.

--- client/chat.js
+++ client/chat.js
@@ -1,5 +1,61 @@
 $(function (){
     let socket = io();
+    let nickname;
+    let color;
+
+    function setCredentials() {
+        // TODO Create entry in the list with nickname and color
+        $("#activeUsers").append("<li id='myNickname' class='list-group-item d-flex justify-content-between align-items-center' style='border-color:" 
+            + color +"'>" + nickname + "<span class='badge badge-secondary badge-pill'>You</span></li>");
+    }
+
+    /*
+    * Setup Function: Configure the user interface upon the page load
+    */
+    $(function(){
+        // Check if there is a Browser Cookie with nickname
+        console.log("Checking for browser cookie");
+        if (document.cookie.split(';').filter(function(item) {
+            return item.trim().indexOf('nickname=') == 0
+        }).length) {
+            // Cookie exists (and therefore client has a pre-existing nickname)
+            nickname = document.cookie.replace(/(?:(?:^|.*;\s*)nickname\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+            console.log("Browser cookie found; nickname = " + nickname);
+            console.log("request color from server");
+            socket.emit('colorRequest', nickname);
+        }
+        else {
+            // No cookie (or pre-existing nickname) 
+            console.log("No Browser Cookie found; requesting nickname and color from server");
+            socket.emit('nicknameAndColorRequest');
+        }
+    });
+
+    socket.on('nicknameAndColor', function(sentNickname, sentColor){
+        // Set Nickname
+        console.log("Nickname received from server; nickname = " + sentNickname);
+        nickname = sentNickname;
+
+        // Set Cookie for Nickname
+        let date = new Date();
+        date.setTime(date.getTime() + (60*60*1000));
+        document.cookie = "nickname=" + nickname + ";" + date;
+
+        // Set Color
+        console.log("Color received from server; color = " + sentColor);
+        color = sentColor;
+
+        // Set Color and Nickname in the GUI
+        setCredentials();
+    });
+
+    socket.on('color', function(sentColor){
+        console.log("Color received from server; color = " + sentColor);
+        color = sentColor;
+
+        // Set Color and Nickname in the GUI
+        setCredentials(); 
+    });
 
     $('form').submit(function(e){
         e.preventDefault(); // prevents page reloading
--- index.js
+++ index.js
@@ -2,6 +2,30 @@ let express = require('express');
 let app = express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
+let nicknames = [
+    "Seahorse",
+    "Octopus",
+    "Squid",
+    "Tuna",
+    "Carp",
+    "Great White",
+    "Hammerhead",
+    "Starfish",
+    "Pufferfish",
+    "Swordfish",
+    "Sea Snake",
+    "Angler",
+    "Sea Turtle",
+    "Rainbow Trout",
+    "Eel",
+    "Clown Fish",
+    "Blue Tang",
+    "Beserker Minnow",
+    "Salmon",
+    "Trout"
+];
+
+let usedNicknames = [];
 
 app.use(express.static(__dirname + "/client"));
 
@@ -9,8 +33,40 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/client/index.html');
 });
 
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
 io.on('connection', function(socket){
-    console.log('a user connected');
+    let nickname;
+    let color;
+
+    // This color generator function based on code written by StackOverflow user "Anatoliy"
+    // https://stackoverflow.com/questions/1484506/random-color-generator
+    function getRandomColor(){
+        let letters = '0123456789ABCDEF';
+        let tempColor = '#';
+        for (var i = 0; i < 6; i++)
+            tempColor += letters[Math.floor(Math.random() * 16)];
+        return tempColor;
+    }
+
+    socket.on('nicknameAndColorRequest', function(){
+        console.log("nickname and color requested");
+        nickname = "tasty boi!"; // TODO Replace this with a nickname generator 
+        color = getRandomColor();
+        socket.emit('nicknameAndColor', nickname, color);
+        console.log("Nickname sent: " + nickname);
+    });
+
+    socket.on('colorRequest', function(clientNickname){
+        console.log("received nickname from client; nickname = " + clientNickname);
+        nickname = clientNickname;
+        console.log("color requested");
+        color = getRandomColor();
+        socket.emit('color', color);
+        console.log("Color Sent: " + color);
+    });
 
     socket.on('disconnect', function(){
         console.log('user disconnected');
@@ -20,8 +76,4 @@ io.on('connection', function(socket){
         console.log('message: ' + msg);
         io.emit('chat message', msg);
     });
-});
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
 });
